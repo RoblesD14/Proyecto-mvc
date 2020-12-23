5caaa5550e1082699acd03298eec31668bf1c061
RoblesD14 <66444366+RoblesD14@users.noreply.github.com>
2020-12-22 20:23:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -28,7 +28,46 @@ include_once "App/vistas/layout/header.php";
 		break;*/
  	default:
  		include_once "App/vistas/uLogin.php";
- 		break;
+		 break;
+		 
+		 case "api/estudiantes":
+			$metodo = $_SERVER["REQUEST_METHOD"];
+			$controladorUsuario = new ControladorUsuario();
+  
+			if ($metodo == "GET"){
+				$resultados = $controladorUsuario->mostrarEstudiantes();
+				$estudiantes[] = null;
+				foreach ($resultados as $key => $estudiante){
+					$estudiantes[$key] = array(
+						"nombres: " => $estudiante["nombres"],
+						"apellidos: " => $estudiante["apellidos"]
+					);
+				}
+				echo json_encode($estudiantes);
+			}
+  
+			if ($metodo== "POST") {
+				$nombres = $_POST["nombres"];
+				$apellidos = $_POST["apellidos"];
+				$codigo = $_POST["codigo"];
+				$password = $_POST["password"];
+				$tipo = $_POST["tipo"];
+				$resultado = $controladorUsuario->crearUsuario($nombres, $apellidos, $codigo, $password, $tipo);
+				if ($resultado == "Guardado") {
+					echo json_encode(array(
+						"codigo" => "200",
+						"status" => "ok"
+					));
+				}
+			}else{
+				echo json_encode(array(
+				  "codigo" => "500",
+				  "status" => "Error"
+				));
+			}
+  
+			break;
+  }
  }
 
 include_once "App/vistas/layout/footer.php";
